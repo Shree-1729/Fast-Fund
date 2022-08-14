2bbeaa9f438b7f9041d7e3d555f2b80686c23442
Shree-1729 <102042064+Shree-1729@users.noreply.github.com>
2022-08-14 05:52:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Component, useState, useEffect } from "react";
+
 import HomePage from "./Components/HomePage";
 import Register from "./Components/Register";
 
